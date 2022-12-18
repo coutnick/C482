8e2ecdd410749fbc3e98c47b8b2a5adf648a7ee7
Nick <nrains1020@gmail.com>
2022-12-18 14:55:21 -0500

    Restarted

--- finalProject/src/main/java/rains/finalproject/HelloController.java
+++ finalProject/src/main/java/rains/finalproject/HelloController.java
@@ -1,23 +1,14 @@
 package rains.finalproject;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class HelloController implements Initializable {
+public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("I am initialized");
     }
 }
